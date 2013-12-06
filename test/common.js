7a4f0f827b3a0d14dd7522aaae035acf4ecd75cb
Jeremial <jeremial90@gmail.com>
2013-12-06 11:57:03 +0800

    update

--- test/common.js
+++ test/common.js
@@ -2,7 +2,7 @@
 global.chai      = require('chai');
 global.sinon     = require('sinon');
 global.expect    = chai.expect;
-global.should    = global.chai.should();
+global.should    = chai.should();
 
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
