21060399b54b98cdb975180722395e10079e56ef
Wyntau <wyntau@outlook.com>
2018-12-15 20:00:38 +0800

    refactor: 调整目录结构

--- test/common.js
+++ test/common.js
@@ -2,7 +2,7 @@
 global.chai      = require('chai');
 global.sinon     = require('sinon');
 global.should    = chai.should();
-global.MVCObject = require('../MVCObject').MVCObject;
+global.MVCObject = require('../').MVCObject;
 
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
