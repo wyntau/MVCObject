3af19aef4db4a11055c9921eb8769baceee0b5ee
Wyntau <wyntau@outlook.com>
2018-12-15 20:10:23 +0800

    chore: use rollup to build

---
+++ build/rollup.config.js
@@ -0,0 +1,49 @@
+import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import {
+  uglify
+} from 'rollup-plugin-uglify';
+
+const resolveOptions = {
+  jsnext: true,
+  main: true,
+  browser: true,
+};
+const typescriptOptions = {
+  typescript: require('typescript'),
+  include: 'src/**',
+  exclude: ['node_modules/**']
+};
+
+// 开发文件
+const bundles = [{
+  input: 'src/mvcobject.ts',
+  output: {
+    file: `dist/mvcobject.js`,
+    name: 'MVCObject',
+    format: 'umd',
+    exports: 'named'
+  },
+  plugins: [
+    resolve(Object.assign({}, resolveOptions)),
+    commonjs(),
+    typescript(Object.assign({}, typescriptOptions))
+  ]
+}, {
+  input: 'src/mvcobject.ts',
+  output: {
+    file: `dist/mvcobject.min.js`,
+    name: 'MVCObject',
+    format: 'umd',
+    exports: 'named'
+  },
+  plugins: [
+    resolve(Object.assign({}, resolveOptions)),
+    commonjs(),
+    typescript(Object.assign({}, typescriptOptions)),
+    uglify()
+  ]
+}];
+
+export default bundles;
